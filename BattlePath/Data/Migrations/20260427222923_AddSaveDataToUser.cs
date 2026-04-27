90145855a50d0cb4f95fe17eb69829535ff2da13
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-04-27 18:38:03 -0400

    Add Save Migrations

---
+++ BattlePath/Data/Migrations/20260427222923_AddSaveDataToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BattlePath.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSaveDataToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SaveDataJson",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SaveDataJson",
+                table: "AspNetUsers");
+        }
+    }
+}
