a770a89f94ca9f521d16d63f256164ebe075020e
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-04-27 18:37:48 -0400

    Add Save and Load methods

--- BattlePath/Data/ApplicationUser.cs
+++ BattlePath/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace BattlePath.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? SaveDataJson { get; set; }
     }
 
 }
