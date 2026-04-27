11325f2c4582f22f287c90148975addfa080c747
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-04-27 17:43:26 -0400

    Fix Save

--- BattlePath/Models/Save.cs
+++ BattlePath/Models/Save.cs
@@ -3,19 +3,19 @@
     public class Save
     {
 
-        public int PlayerHealth { get; set; } = 50;
-        public int PlayerMaxHealth { get; set; } = 50;
+        public int PlayerHealth { get; set; }
+        public int PlayerMaxHealth { get; set; }
         public int PlayerExp { get; set; }
         public int PlayerLvl { get; set; }
 
-        public int PlayerAttack { get; set; } = 10;
+        public int PlayerAttack { get; set; }
 
         public List<Enemy> Enemies { get; set; } = new();
         public Enemy? CurrentEnemy { get; set; }
         public Position Exit { get; set; }
         public List<Position> Waters { get; set; } = new();
         public bool InCombat { get; set; } = false;
-        public int depth { get; set; } = 0;
+        public int depth { get; set; }
 
 
 
